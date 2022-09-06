0d34b54e9fc2143c57a571d7784862420e0dd3a1
LokeshKiran <lokeshkskiran@gmail.com>
2022-09-06 21:47:53 +0530

    [Lokesh]Added:UC2 adding new contact details and displaying the same

---
+++ AddressBookSystem/AddressBookBuilder.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+    internal class AddressBookBuilder
+    {
+        private LinkedList<Contacts> list = new LinkedList<Contacts>();
+        public void AddContact(string firstName, string lastName, string address, string city, string state, string email, int pinCode, long phoneNumber)
+        {
+           Contacts contacts = new Contacts();
+            contacts.firstName=firstName;
+            contacts.lastName=lastName;
+            contacts.address=address;   
+            contacts.city=city;
+            contacts.state=state;
+            contacts.email=email;
+            contacts.pinCode=pinCode;
+            contacts.phoneNumber=phoneNumber;
+            list.AddLast(contacts);
+        }
+        public void DisplayContact()
+        {
+            if(list.Count > 0)
+            {
+                foreach(var contact in list)
+                {
+                    Console.WriteLine("First Name : " + contact.firstName);
+                    Console.WriteLine("Last Name : " + contact.lastName);
+                    Console.WriteLine("Address : " + contact.address);
+                    Console.WriteLine("City : " + contact.city);
+                    Console.WriteLine("State : " + contact.state);
+                    Console.WriteLine("Email : " + contact.email);
+                    Console.WriteLine("Zip : " + contact.pinCode);
+                    Console.WriteLine("Phone Number : " + contact.phoneNumber + "\n");
+                }
+            }
+            else
+                Console.WriteLine("list is empty");
+        }
+
+    }
+}
--- AddressBookSystem/Program.cs
+++ AddressBookSystem/Program.cs
@@ -10,19 +10,50 @@ namespace AddressBookSystem
     {
         static void Main(string[] args)
         {
+            AddressBookBuilder builder = new AddressBookBuilder();
             Console.WriteLine("Welcome To Address Book System");
-            Console.WriteLine("Choose option: \n1.Creating Contacts");
-            int option=int.Parse(Console.ReadLine());
-            switch(option)
+            int option2;
+
+            do
             {
-                case 1:
-                    UC1_AbilityToCreateContacts uC1_AbilityToCreateContacts=new UC1_AbilityToCreateContacts();
-                    uC1_AbilityToCreateContacts.addContact();
-                    break;
-                default:
-                    Console.WriteLine("Please enter correct option");
-                    break;
+                Console.WriteLine("Choose option: \n1.Creating Contacts\n2.Adding New Contacts\n3.Displaying Contacts");
+                int option = int.Parse(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        UC1_AbilityToCreateContacts uC1_AbilityToCreateContacts = new UC1_AbilityToCreateContacts();
+                        uC1_AbilityToCreateContacts.addContact();
+                        break;
+                    case 2:
+                        Console.WriteLine("Enter First Name :");
+                        string firstName = Console.ReadLine();
+                        Console.WriteLine("Enter Last Name :");
+                        string lastName = Console.ReadLine();
+                        Console.WriteLine("Enter Address :");
+                        string address = Console.ReadLine();
+                        Console.WriteLine("Enter City :");
+                        string city = Console.ReadLine();
+                        Console.WriteLine("Enter State :");
+                        string state = Console.ReadLine();
+                        Console.WriteLine("Enter Email :");
+                        string email = Console.ReadLine();
+                        Console.WriteLine("Enter pinCode :");
+                        int pinCode = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter Phone Number :");
+                        long phoneNumber = Convert.ToInt64(Console.ReadLine());
+                        builder.AddContact(firstName, lastName, address, city, state, email, pinCode, phoneNumber);
+                        break;
+                    case 3:
+                        builder.DisplayContact();
+                        break;
+                    default:
+                        Console.WriteLine("Please enter correct option");
+                        break;
+                }
+                Console.WriteLine("Do you want to continue, then press '1' else press '0' to exit");
+                option2 = Convert.ToInt32(Console.ReadLine());
             }
+            while (option2 != 0);
         }
     }
 }
